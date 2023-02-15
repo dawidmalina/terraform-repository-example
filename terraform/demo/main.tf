eed69dc6e1f82185b382746721f22a78c5e74444
Dawid Malinowski <dawid.malinowski@vizlib.com>
2023-02-15 14:32:03 +0100

    Add basic configuration

--- terraform/demo/main.tf
+++ terraform/demo/main.tf
@@ -1,8 +1,23 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.51.0"
+# create demo network
+resource "google_compute_network" "vpc_network" {
+  name = "demo-vpc-network"
+}
+
+
+# create demo instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "demo-terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
     }
   }
 }
