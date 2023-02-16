bb1626573d208589b28187bf213a0a6d006c5056
Dawid Malinowski <dawidmalina@gmail.com>
2023-02-16 10:58:09 +0100

    Update output.tf

--- terraform/demo/output.tf
+++ terraform/demo/output.tf
@@ -1,9 +1,9 @@
-output "instance_id" {
-  description = "An identifier for the resource with format"
-  value       = google_compute_instance.vm_instance.instance_id
-}
+# output "instance_id" {
+#   description = "An identifier for the resource with format"
+#   value       = google_compute_instance.vm_instance.instance_id
+# }
 
-output "ip" {
-  description = "The internal ip address of the instance"
-  value       = google_compute_instance.vm_instance.network_interface.0.network_ip
-}
+# output "ip" {
+#   description = "The internal ip address of the instance"
+#   value       = google_compute_instance.vm_instance.network_interface.0.network_ip
+# }
